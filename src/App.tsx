a37f6f7a4506f5b795e7e99233b506082d27fa77
Yokesh M <Yokesh.M@maplelabs.com>
2023-01-02 12:40:47 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import api from "./api/expenses"
+import Expense from './Components/Expense/Expenses/Expense';
+import NewExpense from './Components/NewExpense/NewExpense/NewExpense';
+
+interface Item {
+  id?: string,
+  title: string,
+  amount: number,
+  date: Date
+};
 
 function App() {
+  const [expenses, setExpenses] = useState<Item[]>([])
+
+  //retrive expense Data
+  const expenseData = async() => {
+    const response = await api.get("/expenses");
+    return response.data;
+  }
+  useEffect(() => {
+    const getExpenseData = async() => {
+      const allExpense = await expenseData();
+      allExpense.map((e: Item) => {
+        return e.date = new Date (e.date)
+      })
+      if (allExpense) setExpenses(allExpense)
+    }
+    getExpenseData();
+  }, [])
+
+  const addExpenseHandler = async(expense: Item) => {
+    const response = await api.post("/expenses", expense)
+    setExpenses((prevExpenses) => {
+      response.data.date = new Date (response.data.date)
+      return [response.data, ...prevExpenses]
+    })
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler}/>
+      <Expense expenses={expenses}/>
     </div>
   );
 }
